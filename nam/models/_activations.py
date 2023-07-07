2fa451200066974a2ccea19a1a88f0ffe9532498
d-Vincent-b <132890373+d-Vincent-b@users.noreply.github.com>
2023-07-07 23:26:33 +0200

    Delete nam/models directory

--- nam/models/_activations.py
+++
@@ -1,9 +0,0 @@
-# File: _activations.py
-# Created Date: Friday July 29th 2022
-# Author: Steven Atkinson (steven@atkinson.mn)
-
-import torch.nn as nn
-
-
-def get_activation(name: str) -> nn.Module:
-    return getattr(nn, name)()
